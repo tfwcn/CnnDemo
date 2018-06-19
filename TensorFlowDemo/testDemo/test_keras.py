0d3c81d42ae171832d09975f6b6e90df5c8835b6
tfwcn@sina.cn <tfwcn@sina.cn>
2018-06-20 07:48:53 +0800

    update

--- TensorFlowDemo/testDemo/test_keras.py
+++ TensorFlowDemo/testDemo/test_keras.py
@@ -22,13 +22,13 @@ def dense_to_one_hot(labels_dense, num_classes=2):
 
 def readFiles(file_dir):
     # 读取文件列表
-    # file_dir="/home/hillsun/dxh/images_hyj_7243/man_4314/"
-    # file_dir="/home/hillsun/dxh/images_yzr_avg/"
-    # pathDir = os.listdir(file_dir)[0:2900]
-    pathDir = os.listdir(file_dir)
-    # 定义特征与标签
-    TEST_IMAGE_PATHS = [file_dir+path for path in pathDir]
-    TEST_GENDER = [int(path.split('_')[1])-1 for path in pathDir]
+    TEST_IMAGE_PATHS = []
+    TEST_GENDER = []
+    for i in range(len(file_dir)):
+        pathDir = os.listdir(file_dir[i])
+        # 定义特征与标签
+        TEST_IMAGE_PATHS += [file_dir[i]+path for path in pathDir]
+        TEST_GENDER += [int(path.split('_')[1])-1 for path in pathDir]
     # # 读取文件列表
     # file_dir="/home/hillsun/dxh/images_hyj_7243/women_2929/"
     # pathDir = os.listdir(file_dir)[0:2900]
@@ -55,15 +55,15 @@ def readFiles(file_dir):
     # image = tf.image.grayscale_to_rgb(image)#转彩色
     image = tf.image.resize_images(image, [160, 160])  # 缩放图片
     # 随机
-    # num_preprocess_threads = 4  # 读取线程数
-    # batch_size = 50  # 每次训练数据量
-    # min_queue_examples = 5000  # 最小数据量
-    # image, label = tf.train.shuffle_batch(
-    #     [image, label],
-    #     batch_size=batch_size,
-    #     num_threads=num_preprocess_threads,
-    #     capacity=min_queue_examples + 3 * batch_size,
-    #     min_after_dequeue=min_queue_examples)
+    num_preprocess_threads = 4  # 读取线程数
+    batch_size = 50  # 每次训练数据量
+    min_queue_examples = 5000  # 最小数据量
+    image, label = tf.train.shuffle_batch(
+        [image, label],
+        batch_size=batch_size,
+        num_threads=num_preprocess_threads,
+        capacity=min_queue_examples + 3 * batch_size,
+        min_after_dequeue=min_queue_examples)
     # print(label._shape)
     # print(image._shape)
     image = tf.divide(image, 255.0)
@@ -83,30 +83,37 @@ def readFiles2():
     image = tf.divide(image, 255.0)
     return filename, image
 
+def generate_arrays_from_file(image, label):  
+    with tf.Session() as sess:
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        while 1:  
+            image_value, label_value = sess.run([image, label])
+            yield (image_value, label_value)  
 
 def train():
     """训练"""
-    file_dir = "/home/hillsun/dxh/images_yzr_avg/"
+    file_dir = ["E:/face0/女_2929/", "E:/face0/男_2929/"]
     label, image = readFiles(file_dir)  # 读取文件
-    file_dir2 = "/home/hillsun/dxh/images_align_240/"
+    file_dir2 = ["E:/face0/女_250/", "E:/face0/男_250/"]
     label2, image2 = readFiles(file_dir2)  # 读取文件
     keep_prob = 0.6  # dropout概率
     model = Sequential()
-    model.add(Conv2D(96, 7, 7, (4, 4)))
+    model.add(Conv2D(96, (7, 7), strides=(4, 4)))
     model.add(Activation(backend.relu))
-    model.add(MaxPool2D((3, 3), (2, 2)))
+    model.add(MaxPool2D((3, 3), strides=(2, 2)))
     model.add(BatchNormalization())
     model.add(Dropout(keep_prob))
 
-    model.add(Conv2D(80, 5, 5))
+    model.add(Conv2D(80, (5, 5)))
     model.add(Activation(backend.relu))
-    model.add(MaxPool2D((3, 3), (2, 2)))
+    model.add(MaxPool2D((3, 3), strides=(2, 2)))
     model.add(BatchNormalization())
     model.add(Dropout(keep_prob))
 
-    model.add(Conv2D(120, 5, 5))
+    model.add(Conv2D(120, (5, 5)))
     model.add(Activation(backend.relu))
-    model.add(MaxPool2D((3, 3), (2, 2)))
+    model.add(MaxPool2D((3, 3), strides=(2, 2)))
     model.add(BatchNormalization())
     model.add(Dropout(keep_prob))
 
@@ -119,17 +126,18 @@ def train():
     model.add(Activation(backend.softmax))
 
     # 编译模型
-    model.compile(optimizers.Adadelta,
+    model.compile(optimizers.Adadelta(),
                   losses.categorical_crossentropy, metrics.binary_accuracy)
     # 训练
-    model.fit(image, label, batch_size=50, epochs=1000,
-              verbose=1, validation_data=(image2, label2))
+    # model.fit(image_value, label_value, batch_size=50, epochs=1000,
+    #         verbose=1, validation_data=(image2_value, label2_value))
+    model.fit_generator(generate_arrays_from_file(image, label),samples_per_epoch=10000, nb_epoch=10)
     # 识别
-    score = model.evaluate(image2, label2, verbose=1)
+    # score = model.evaluate(image2_value, label2_value, verbose=1)
+    score = model.evaluate_generator(generate_arrays_from_file(image2, label2))
 
-    print("损失值：", scorep[0])
-    print("准确率：", scorep[1])
-
+    print("损失值：", score[0])
+    print("准确率：", score[1])
     return
 
 
