46155dfdc0f3d8aeb0a1632ecc98cab6b1412a42
hyj <hanyongjian@hsv-tec.com>
2018-11-02 17:54:54 +0800

    test

--- TensorFlowDemo/testDemo/test_maskrcnn.py
+++ TensorFlowDemo/testDemo/test_maskrcnn.py
@@ -5,11 +5,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 
+class MyMaskRCNN():
+
+    def __init__(self,label_path):
+        self.label_path = label_path
+
+    def creade_model():
+        """创建模型"""
+        
+
+
+# 读取启动参数
 parser = argparse.ArgumentParser()
-parser.add_argument('face_path')
+parser.add_argument('label_path')
 parser.add_argument('-t', '--train', default="1")
 args = parser.parse_args()
 
-face_path = args.face_path
-
-
+# 数据集路径
+label_path = args.label_path
